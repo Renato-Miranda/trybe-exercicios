271f6defc755c9e8e9cb58346997c130d19b42ac
Renato-Miranda <renatomna@gmail.com>
2023-01-23 16:13:38 -0300

    criado arquivo if/else

---
+++ fundamentos/secao03-introducaoJS-dia01/if-else.js
@@ -0,0 +1,11 @@
+const nota = 90;
+
+if (nota >= 80){
+    console.log("parabens")
+}else if (nota < 80 && nota >= 60){
+    console.log("aprovado" )
+}else if(nota < 60){
+    console.log("reprovado")
+}else{
+    console.log("inexistente") 
+}
