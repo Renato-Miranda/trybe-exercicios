5b5a76266112a679b653b944e68d35d53d219fc4
Renato-Miranda <renatomna@gmail.com>
2023-01-23 18:52:24 -0300

    adicionando exercicio 03

--- fundamentos/secao03-introducaoJS-dia01/exercicios-pos-aula.js
+++ fundamentos/secao03-introducaoJS-dia01/exercicios-pos-aula.js
@@ -1,6 +1,9 @@
 // exercicio 01
 const a = 50;
-const b = 12;
+const b = 70;
+const c = 2;
+
+let result;
 /*
 console.log(a+b);
 console.log(a-b);
@@ -14,5 +17,13 @@ console.log(a%b);
 }else{
     console.log(b);
 }*/
-
+// exercicio 03
+/*
+if (a > b && a > c){
+    console.log(a);
+}else if(b > c && b > a){
+    console.log(b);
+}else {
+    console.log(c);
+}*/
 
