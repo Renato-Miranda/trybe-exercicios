bc500d0af141831f4c8531a855d05414d1228f88
Renato-Miranda <renatomna@gmail.com>
2023-01-24 18:39:51 -0300

    utilizando o for para descobrir o maior valor da array

--- fundamentos/sessao03-introducaoJS-dia02/Lidando-com-arrays.js
+++ fundamentos/sessao03-introducaoJS-dia02/Lidando-com-arrays.js
@@ -8,7 +8,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let sum = 0;
 
-for (i = 0; i < numbers.length; i+=1) {
+for (let i = 0; i < numbers.length; i+=1) {
     sum += numbers[i];
 }
 
@@ -18,15 +18,24 @@ for (i = 0; i < numbers.length; i+=1) {
 
 let media = 0;
 
-for(i = 0; i < numbers.length; i+=1) {
-    media +=  numbers[i] / numbers.length;
-}
+// for(let i = 0; i < numbers.length; i+=1) {
+//     media +=  numbers[i] / numbers.length;
+// }
 
 
-    if(media > 20) {
-        console.log('valor maior que 20');
-    }else{
-        console.log('valor menor ou igual a 20');
-    }
+//     if(media > 20) {
+//         console.log('valor maior que 20');
+//     }else{
+//         console.log('valor menor ou igual a 20');
+//     }
 
 // console.log(media);
+
+let maiorValor = 0;
+
+for(let i = 0; i < numbers.length; i+=1) {
+    if(numbers[i] > maiorValor) {
+        maiorValor = numbers[i]
+    }
+}
+console.log(maiorValor)
