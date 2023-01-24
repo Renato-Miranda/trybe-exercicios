1dd67c0e6b6c456c606d62111c7aa7da8fea8dcb
Renato-Miranda <renatomna@gmail.com>
2023-01-24 10:28:09 -0300

    feito exercicio valor porcentagem produtos

--- fundamentos/secao03-introducaoJS-dia01/exercicio-foguete.js
+++ fundamentos/secao03-introducaoJS-dia01/exercicio-foguete.js
@@ -1,4 +1,4 @@
-const a = 61;
+/*const a = 61;
 const b = 60;
 const c = 60;
 
@@ -8,3 +8,15 @@ if(confere % 2 !== 0){
 }else{
     console.log(false);
 }
+*/
+
+const custoProd = -2;
+const valorVenda = 200;
+
+if (custoProd >=0 && valorVenda >=0){
+    const custoTotalProd = custoProd * 1.2;
+    const lucroTotal = (valorVenda - custoTotalProd) * 1000;
+    console.log(lucroTotal);
+}else{
+    console.log("Erro, Valor negativo!")
+};
