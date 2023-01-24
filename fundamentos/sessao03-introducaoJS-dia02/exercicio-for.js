41785c7ad7d822863d9e304e36646c18c77839b6
Renato-Miranda <renatomna@gmail.com>
2023-01-24 12:04:41 -0300

    adicionando exercicios de for e array

---
+++ fundamentos/sessao03-introducaoJS-dia02/exercicio-for.js
@@ -0,0 +1,12 @@
+/*let numero = 7;
+
+for( let index = 1; index <= 9; index = index +1){
+    console.log(numero * index);
+}*/
+
+let listaNomes =['Joana', 'Maria', 'Lucas'];
+
+for(let index = 0; index < listaNomes.length; index++){
+    let mensagem = "Boas Vindas, " + listaNomes[index] + '!';
+    console.log(mensagem)
+}
