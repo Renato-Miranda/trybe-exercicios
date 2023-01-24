6bb30e6ffd81cd9b397f858abc6be6e920d1d9d5
Renato-Miranda <renatomna@gmail.com>
2023-01-24 18:07:37 -0300

    adicionando somo entre os numeros da array

--- fundamentos/sessao03-introducaoJS-dia02/Lidando-com-arrays.js
+++ fundamentos/sessao03-introducaoJS-dia02/Lidando-com-arrays.js
@@ -1,3 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(numbers);
+// console.log(numbers);
+
+let sum = 0;
+
+for (i = 0; i < numbers.length; i+=1) {
+    sum += numbers[i];
+}
+
+console.log(sum)
