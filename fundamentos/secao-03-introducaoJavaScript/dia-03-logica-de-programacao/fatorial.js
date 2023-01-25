fc888b2ca11c272c477f7d5bfc1cbb108ccb589f
Renato-Miranda <renatomna@gmail.com>
2023-01-25 13:41:30 -0300

    feito dois primeiros exercicios

---
+++ fundamentos/secao-03-introducaoJavaScript/dia-03-logica-de-programacao/fatorial.js
@@ -0,0 +1,23 @@
+// 1- crie um algoritmo que imprima na tela o fatorial de 10.
+
+// let fatorial = 1;
+
+// for (let i = 10; i > 0; i -= 1) {
+//     fatorial *= i;
+// }
+
+// console.log(fatorial);
+
+// 2 - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de 
+// inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. 
+// Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu 
+// algoritmo está funcionando corretamente.
+
+// let word = 'tryber';
+// let invert = "";
+// for (let i = 0; i < word.length; i += 1) {
+//     invert += word[word.length - 1 - i];
+// }
+// console.log(invert)
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
