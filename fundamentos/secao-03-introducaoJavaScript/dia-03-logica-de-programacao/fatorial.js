0c8ee879614c2f9a564d1f489f82347b3cf722fe
Renato-Miranda <renatomna@gmail.com>
2023-01-25 15:31:12 -0300

    criado exercicio de meior e menor palavra

--- fundamentos/secao-03-introducaoJavaScript/dia-03-logica-de-programacao/fatorial.js
+++ fundamentos/secao-03-introducaoJavaScript/dia-03-logica-de-programacao/fatorial.js
@@ -21,3 +21,21 @@
 // console.log(invert)
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > maiorPalavra) {
+        maiorPalavra = array[i];
+    }
+}
+
+for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length < menorPalavra) {
+        menorPalavra = array[i];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
